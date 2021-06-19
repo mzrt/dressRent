42cc2a3c5cb6c837f016828406c5a0af50d79aa7
Evgeniy Tortumashev <johnnytort@yandex.ru>
2021-06-19 04:05:05 +0000

    Add app.js & config

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Run server...");
+const express = require("express");
+const config = require("config");
+
+const PORT = config.get("port");
+
+const app = express();
+app.listen(PORT, () => {
+console.log(`App has been started on port ${PORT}...`);
+});
